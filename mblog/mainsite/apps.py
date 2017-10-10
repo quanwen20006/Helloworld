104a82013ae2c2e982174b90b185c0351f53f807
wen.quan <wen.quan@intellgentgroup.cn>
2017-10-10 15:33:13 +0800

    更新提交版本，功能实现blog

--- mblog/mainsite/apps.py
+++ mblog/mainsite/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class BlogConfig(AppConfig):
-    name = 'blog'
+class MainsiteConfig(AppConfig):
+    name = 'mainsite'
