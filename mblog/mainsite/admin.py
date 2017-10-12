f42dff5d6045a70299ab24b3b956f51b649221df
wen.quan <wen.quan@intellgentgroup.cn>
2017-10-12 14:58:47 +0800

    上传确认版blog系统，数据库使用的是mysql

--- mblog/mainsite/admin.py
+++ mblog/mainsite/admin.py
@@ -3,7 +3,8 @@ from .models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'pub_date')
+    list_display = ('title', 'slug', 'atype', 'pub_date')
+    list_filter = ('atype', 'pub_date')
 
 
 admin.site.register(Article, ArticleAdmin)
--- mblog/mainsite/models.py
+++ mblog/mainsite/models.py
@@ -1,13 +1,16 @@
+# -*- coding:utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 from django.utils import timezone
+
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
+    articletype = (('auto', '自动化测试'), ('safe', '安全测试'), ('per', '性能测试'), ('busi', '业务'))
     body = models.TextField()
     pub_date = models.DateTimeField(default=timezone.now)
+    atype = models.CharField(max_length=10, choices=articletype, default='auto')
 
     class Meta:
         ordering = ('pub_date',)
